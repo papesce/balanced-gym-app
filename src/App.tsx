6822a040c5e8f0a4a472bc7226395c032bf9bd94
Pablo Pesce <pablo.pesce@ibm.com>
2019-11-11 23:19:56 -0300

    add list

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import store from './redux/configureStore';
 import Header from './components/Header';
+import List from './components/List';
 
 const App: React.FC = () => {
   return (
     <Provider store={store}>
       <Header/>
+      <List />
     </Provider>
   );
 }
