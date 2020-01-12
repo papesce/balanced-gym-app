ef5358c8e9da876d0902a673150df186d218a236
Pablo Pesce <papesce@gmail.com>
2020-01-12 20:44:19 -0300

    add tooltips

--- src/components/logPage/graph/GraphBar.tsx
+++ src/components/logPage/graph/GraphBar.tsx
@@ -2,27 +2,43 @@ import React, { Component } from "react";
 import IconButton from "@material-ui/core/IconButton";
 import EditOutlinedIcon from "@material-ui/icons/EditOutlined";
 import MoreOutlinedIcon from "@material-ui/icons/MoreOutlined";
+import Tooltip from "@material-ui/core/Tooltip";
 import "./GraphBar.css";
 
 interface GraphBarProps {
-    edit?: boolean;
-    more?: boolean;
-    handleEditClick?: () => void;
-    handleMoreClick?: () => void;
+  edit?: boolean;
+  more?: boolean;
+  handleEditClick?: () => void;
+  handleMoreClick?: () => void;
 }
 
 export default class GraphBar extends Component<GraphBarProps> {
   render() {
-    const {edit, more, handleEditClick, handleMoreClick} = this.props;
+    const { edit, more, handleEditClick, handleMoreClick } = this.props;
     return (
       <div className="graph-bar-container">
-        {edit && <IconButton className={"graph-bar-icon-button"} size="small" onClick={handleEditClick}>
-          <EditOutlinedIcon fontSize="inherit" />
-        </IconButton>}
-        {more && <IconButton className={"graph-bar-icon-button"} size="small" onClick={handleMoreClick}>
-          <MoreOutlinedIcon fontSize="inherit" />
-        </IconButton>
-        }
+        {edit && (
+          <Tooltip title="Edit Series">
+            <IconButton
+              className={"graph-bar-icon-button"}
+              size="small"
+              onClick={handleEditClick}
+            >
+              <EditOutlinedIcon fontSize="inherit" />
+            </IconButton>
+          </Tooltip>
+        )}
+        {more && (
+          <Tooltip title="Show More">
+            <IconButton
+              className={"graph-bar-icon-button"}
+              size="small"
+              onClick={handleMoreClick}
+            >
+              <MoreOutlinedIcon fontSize="inherit" />
+            </IconButton>
+          </Tooltip>
+        )}
       </div>
     );
   }
