47f246c3c10c7293adf8e45bf668a99275c63529
Pablo Pesce <papesce@gmail.com>
2020-02-09 18:03:05 -0300

    fix build script again

--- packages/server/build.ts
+++ packages/server/build.ts
@@ -5,5 +5,5 @@ const outDir = config.compilerOptions.outDir;
 s.rm('-rf', outDir);
 s.mkdir(outDir);
 s.cp('.env', `${outDir}/.env`);
-s.mkdir('-p', `${outDir}/common/swagger`);
+s.mkdir('-p', `${outDir}/api/swagger`);
 s.cp('src/api/swagger/Api.yaml', `${outDir}/api/swagger/Api.yaml`);
