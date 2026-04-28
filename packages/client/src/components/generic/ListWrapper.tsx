a87705121745744863d74818d6458bf242f98288
Pablo Pesce <papesce@gmail.com>
2026-04-28 19:48:02 -0300

    chore: modernize dev setup for Node 22 and clean up gitignore

--- packages/client/src/components/generic/ListWrapper.tsx
+++ packages/client/src/components/generic/ListWrapper.tsx
@@ -6,7 +6,7 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles(theme => ({
     root: {
       width: '100%',
-      maxWidth: 360,
+      maxWidth: '100%',
       backgroundColor: theme.palette.background.paper,
     },
   }));
