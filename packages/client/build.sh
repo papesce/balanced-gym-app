a76e126b649cb4026cf9229fe91633351a73e8e5
Pablo Pesce <papesce@gmail.com>
2026-05-01 09:40:26 -0300

    fixed issues with thee build

--- packages/client/build.sh
+++ packages/client/build.sh
@@ -5,4 +5,4 @@ mkdir ../server/public;
 cp -a ../server/api-docs/. ../server/public/api-docs;
 cp -a ../server/assets/. ../server/public/assets;
 cd build
-mv * ../../server/public
+cp -a . ../../server/public
